91e5b5f9da79b91972d28627fdd92d9170c7eed6
AMkuro <4324018@ed.tus.ac.jp>
2026-03-21 20:28:16 +0900

    start writing ex1

---
+++ ex1/data_stream.py
@@ -0,0 +1,41 @@
+from typing import Any, List, Dict, Union, Optional  # noqa
+from abc import ABC, abstractmethod
+
+
+class DataStream(ABC):
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(
+        self, data_batch: List[Any], criteria: Optional[str] = None
+    ) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+class SensorStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return super().process_batch(data_batch)
+
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+
+class EventStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+
+
+class StreamProcessor:
+    pass
