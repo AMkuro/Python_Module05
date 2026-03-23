811774c51aa9771208e87bfd761b8da92621d8ae
AMkuro <4324018@ed.tus.ac.jp>
2026-03-23 19:13:46 +0900

    move to other place. Just for sharing

--- ex2/nexus_pipeline.py
+++ ex2/nexus_pipeline.py
@@ -1,32 +1,39 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Dict, Protocol, Union, Optional  # noqa
+from typing import Any, List, Dict, Protocol, Union, Optional
 
 class ProcessingStage(Protocol):
     def process(self,data:Any) -> Any:
-        pass
+        pass 
 
 class InputStage:
-    def process(data) -> Dict:
-        pass
+    def process(self,data:Any) -> Dict:
+        if data is None:
+            raise ValueError("Data input invalid. Data must contain at least one value.")
+        try:
+            match data:
+                case dict():
+                    return data
+        except (ValueError, AttributeError):
+            raise
 
 class TransformStage:
-    def process(data) -> Dict:
+    def process(self,data:Dict) -> Dict:
         pass
 
 class OutputStage:
-    def process(data) ->str:
+    def process(self,data) ->str:
         pass
 
 class ProcessingPipeline(ABC):
     def __init__(self,pipeline_id: str) -> None:
-        self.stages: List[Stage] = []
-        self.pipeline_id: str = ""
+        self.stages: List[ProcessingStage] = []
+        self.pipeline_id: str = pipeline_id
 
     @abstractmethod
     def process(self, data: Any) -> Union[str, Any]:
         pass
 
-    def add_stage():
+    def add_stage(self):
         pass
 
 
@@ -54,14 +61,19 @@ class NexusManager:
     def __init__(self) -> None:
         self.pipelines: List[ProcessingPipeline] =[]
 
-    def add_pipeline():
+    def add_pipeline(self):
         pass
 
-    def process_data():
+    def process_data(self):
         pass
+
 def demo_pipeline_system() -> None:
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
-
+    manager = NexusManager()
 
 def main() -> None:
-    demo_pipeline_system()
+    # demo_pipeline_system()
+    sample_str: str = "user,action,timestamp"
+
+if __name__ == "__main__":
+    main()
