dcb8b8b09f0701ee7646b500d9a91c46824017d9
miwasaki <4324018@ed.tus.ac.jp>
2026-03-23 21:40:21 +0900

    linter

--- ex2/nexus_pipeline.py
+++ ex2/nexus_pipeline.py
@@ -1,14 +1,19 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Protocol, Union, Optional
+import collections  # noqa
+
 
 class ProcessingStage(Protocol):
-    def process(self,data:Any) -> Any:
-        pass 
+    def process(self, data: Any) -> Any:
+        pass
+
 
 class InputStage:
-    def process(self,data:Any) -> Dict:
+    def process(self, data: Any) -> Dict:
         if data is None:
-            raise ValueError("Data input invalid. Data must contain at least one value.")
+            raise ValueError(
+                "Data input invalid. Data must contain at least one value."
+            )
         try:
             match data:
                 case dict():
@@ -16,16 +21,19 @@ class InputStage:
         except (ValueError, AttributeError):
             raise
 
+
 class TransformStage:
-    def process(self,data:Dict) -> Dict:
+    def process(self, data: Dict) -> Dict:
         pass
+
 
 class OutputStage:
-    def process(self,data) ->str:
+    def process(self, data) -> str:
         pass
 
+
 class ProcessingPipeline(ABC):
-    def __init__(self,pipeline_id: str) -> None:
+    def __init__(self, pipeline_id: str) -> None:
         self.stages: List[ProcessingStage] = []
         self.pipeline_id: str = pipeline_id
 
@@ -41,25 +49,29 @@ class JSONAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str) -> None:
         super().__init__(pipeline_id)
 
-    def process(self,data: Any) -> Union[str, Any]:
+    def process(self, data: Any) -> Union[str, Any]:
         return super().process(data)
 
 
 class CSVAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str) -> None:
         super().__init__(pipeline_id)
-    def process(self,data: Any) -> Union[str, Any]:
+
+    def process(self, data: Any) -> Union[str, Any]:
         return super().process(data)
+
 
 class StreamAdapter(ProcessingPipeline):
     def __init__(self, pipeline_id: str) -> None:
         super().__init__(pipeline_id)
+
     def process(self, data: Any) -> Union[str, Any]:
         return super().process(data)
 
+
 class NexusManager:
     def __init__(self) -> None:
-        self.pipelines: List[ProcessingPipeline] =[]
+        self.pipelines: List[ProcessingPipeline] = []
 
     def add_pipeline(self):
         pass
@@ -67,13 +79,16 @@ class NexusManager:
     def process_data(self):
         pass
 
+
 def demo_pipeline_system() -> None:
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
     manager = NexusManager()
 
+
 def main() -> None:
     # demo_pipeline_system()
     sample_str: str = "user,action,timestamp"
+
 
 if __name__ == "__main__":
     main()
